62eca5f3021a31705b9d26e1baf8fc3f9e1ce76f
safin-sys <mdsafinofficial@gmail.com>
2021-05-03 06:46:36 +0600

    marquee

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,11 @@
-import style from '../styles/Home.module.scss'
+import style from '../styles/Hero.module.scss'
 
 export const Hero = () => {
     return (
         <div className={style.hero}>
             <h1>Start Learning Nato Phoenitic Alphabets Right Now.</h1>
             <button>Click Here To Start Learning Now</button>
+            <span></span>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,47 @@
 import Head from 'next/head'
 import { Hero } from '../components/Hero'
 import { Navbar } from '../components/Navbar'
-import styles from '../styles/Home.module.scss'
+import Marquee from 'react-fast-marquee'
 
 export const getStaticProps = async () => {
-  const res = await fetch('http://localhost:3000/api/nato')
-  const data = await res.json()
+    const res = await fetch('http://localhost:3000/api/nato')
+    const data = await res.json()
 
-  return {
-    props: {
-      nato: data
+    return {
+        props: {
+            nato: data
+        }
     }
-  }
 }
 
 export default function Home({ nato }) {
-  return (
-    <div className="container">
-      <Head>
-        <title>Zapomnit' NATO</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <Hero />
-    </div>
-  )
+    return (
+        <>
+            <div className="container">
+                <Head>
+                    <title>Zapomnit' NATO</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <Navbar />
+                <Hero />
+            </div>
+            <div className="marq">
+            <Marquee gradient={false} speed={50}>
+                {nato.map((alph, i) => {
+                    return <span key={i}>{alph}</span>
+                })}
+            </Marquee>
+            <Marquee gradient={false} direction="right" speed={50}>
+                {nato.map((alph, i) => {
+                    return <span key={i}>{alph}</span>
+                })}
+            </Marquee>
+            <Marquee gradient={false} speed={50}>
+                {nato.map((alph, i) => {
+                    return <span key={i}>{alph}</span>
+                })}
+            </Marquee>
+            </div>
+        </>
+    )
 }
