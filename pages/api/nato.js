b03889a025c15da168429bf0718dcd52aa513958
safin-sys <mdsafinofficial@gmail.com>
2021-05-03 08:14:39 +0600

    fuck next api route

--- pages/api/nato.js
+++
@@ -1,5 +0,0 @@
-const nato = ['Alpha', 'Bravo', 'Charlie', 'Delta', 'Echo', 'Foxtrot', 'Golf', 'Hotel', 'India', 'Juliett', 'Kilo', 'Lima', 'Mike', 'November', 'Oscar', 'Papa', 'Quebec', 'Romeo', 'Sierra', 'Tango', 'Uniform', 'Victor', 'Whisky', 'X-ray', 'Yankee', 'Zulu']
-
-export default (req, res) => {
-  res.status(200).json(nato)
-}
