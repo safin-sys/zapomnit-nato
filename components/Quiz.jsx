b8f93ddd22f5046f4bf4f807fb10deb8146195bb
safin-sys <mdsafinofficial@gmail.com>
2021-05-31 23:27:23 +0600

    fini

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react"
+import Reward from "react-rewards"
 import style from "../styles/Quiz.module.scss"
 
 export const Quiz = ({ mcqs }) => {
@@ -20,20 +21,27 @@ export const Quiz = ({ mcqs }) => {
 
 const Question = ({ mcqs, setScore, setQuestionNumber }) => {
     const [selected, setSelected] = useState()
+    const [thatsRight] = useState(typeof Audio !== "undefined" && new Audio(`audio/thats right.mp3`));
+    const [sike] = useState(typeof Audio !== "undefined" && new Audio(`audio/sike.mp3`));
     const [mcq, setMCQ] = useState(mcqs[Math.floor(Math.random() * mcqs.length)])
-    
+    const reward = useRef()
+
     const handleAnswer = () => {
-        const options = document.querySelector('.'+ style.options).childNodes
-        if(selected && selected[0] === mcq?.ans[0]) {
-            if(selected === mcq.ans) {
+        const options = document.querySelector('.' + style.options).childNodes
+        if (selected && selected[0] === mcq?.ans[0]) {
+            if (selected === mcq.ans) {
                 setQuestionNumber(n => n + 1)
                 setScore(n => n + 1)
                 setMCQ(mcqs[Math.floor(Math.random() * mcqs.length)])
                 options.forEach(node => {
                     node.classList.remove(style.selected)
                 })
+                reward.current.rewardMe()
+                thatsRight.play()
             } else {
                 setQuestionNumber(n => n + 1)
+                reward.current.punishMe()
+                sike.play()
             }
         } else {
             alert('Select an answer');
@@ -49,10 +57,12 @@ const Question = ({ mcqs, setScore, setQuestionNumber }) => {
                 {mcq.options?.map((opt, key) => {
                     return <Option key={key} i={key} opt={opt} setSelected={setSelected} />
                 })}
-                <button onClick={handleAnswer}>
-                    Next
+                <Reward ref={reward} type="confetti" config={{angle: 70}}>
+                    <button onClick={handleAnswer}>
+                        Next
                         <svg viewBox="64 64 896 896" focusable="false" data-icon="right" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path></svg>
-                </button>
+                    </button>
+                </Reward>
             </div>
         </>
     )
